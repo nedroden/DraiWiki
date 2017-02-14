3615ebd76fc0320d4a6ec37a57a034fde7600cc3
Robert Monden <cj_robert@outlook.com>
2017-02-14 18:06:03 +0100

    Added error page locale

---
+++ lang/en_US/Error.language.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * en_US
+ * DraiWiki language pack
+ *
+ * DraiWiki:
+ * @author		Robert Monden
+ * @copyright	DraiWiki development team, 2017
+ * @version		1.0 Alpha 1
+ * @license		Apache 2.0
+ *
+ * Language pack:
+ * @dialect		General American English
+ * @country 	United States
+ * @translator	Robert Monden
+ * @license		Apache 2.0
+ */
+
+return [
+
+];
