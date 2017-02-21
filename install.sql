5272cb2e698326efbfb17c944957461262990dd2
Robert Monden <dev@robertmonden.com>
2017-02-21 12:04:06 +0100

    Forgot to update the SQL file

--- install.sql
+++ install.sql
@@ -17,7 +17,8 @@ CREATE TABLE `drai_articles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `drai_articles` (`ID`, `title`, `language`, `group_ID`, `status`) VALUES
-(1,	'Homepage',	'en_US',	1,	1);
+(1, 'Homepage', 'en_US',  1,  1),
+(2, 'About',  'en_US',  2,  1);
 
 CREATE TABLE `drai_config` (
   `category` tinytext NOT NULL,
@@ -26,13 +27,13 @@ CREATE TABLE `drai_config` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `drai_config` (`category`, `identifier`, `value`) VALUES
-('wiki',	'WIKI_LOCALE',	'en_US'),
-('wiki',	'WIKI_NAME',	'DraiWiki'),
-('wiki',	'WIKI_SLOGAN',	'Revolutionary wiki software'),
-('wiki',	'WIKI_SKIN',	'default'),
-('wiki',	'WIKI_IMAGES',	'default'),
-('wiki',	'WIKI_TEMPLATES',	'default'),
-('wiki',	'WIKI_HOMEPAGE',	'Homepage');
+('wiki',  'WIKI_LOCALE',  'en_US'),
+('wiki',  'WIKI_NAME',  'DraiWiki'),
+('wiki',  'WIKI_SLOGAN',  'Revolutionary wiki software'),
+('wiki',  'WIKI_SKIN',  'default'),
+('wiki',  'WIKI_IMAGES',  'default'),
+('wiki',  'WIKI_TEMPLATES', 'default'),
+('wiki',  'WIKI_HOMEPAGE',  'Homepage');
 
 CREATE TABLE `drai_history` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -40,10 +41,12 @@ CREATE TABLE `drai_history` (
   `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `date_edited` datetime NOT NULL,
   `edited_by` int(11) NOT NULL,
+  `infobox_ID` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `drai_history` (`ID`, `article_ID`, `body`, `date_edited`, `edited_by`) VALUES
-(1,	1,	'Welcome to DraiWiki! This is just a test.',	'2017-02-20 19:46:38',	1);
+INSERT INTO `drai_history` (`ID`, `article_ID`, `body`, `date_edited`, `edited_by`, `infobox_ID`) VALUES
+(1, 1,  '## 1. Introduction to DraiWiki\r\n### 1.1. What is DraiWiki?\r\nDraiWiki is an upcoming open source wiki software that is designed to be customizable, neat-looking, secure and easy to use.\r\n\r\n### 1.2. Why use DraiWiki?\r\nThere are other free wiki softwares out there, so you might be wondering, what makes DraiWiki the best choice for your website? Well, there are several reasons.\r\n\r\nFirst of all, the software is designed to be customizable. For example, a theme consists of three parts: images, CSS and templates. Basically, what you\'ll be able to do is this: you can use the image set from the default theme, while using the CSS of a 3rd party theme, while using the templates of yet another 3rd party theme. And the best thing is: it\'ll only take a few seconds to set up.\r\n\r\nIt also has built-in multi-language support, meaning you won\'t need an extension.\r\n\r\n## 2. Installation\r\n### 2.1. Server requirements\r\n* PHP 5.6+\r\n* MySQL\r\n* PDO extension',  '2017-02-20 19:46:38',  1,  0),
+(2, 2,  'We\'re cool.', '2017-02-21 11:48:45',  1,  0);
 
--- 2017-02-20 19:15:05
+-- 2017-02-21 11:03:33
