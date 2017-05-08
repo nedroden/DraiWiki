58c77f85ecc7be12216b5db3a4ee6df9e5f458a8
Robert Monden <dev@robertmonden.com>
2017-05-08 19:27:32 +0200

    Readded some files we could still use

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * DRAIWIKI
+ * Open source wiki software
+ *
+ * @version     1.0 Alpha 1
+ * @author      Robert Monden
+ * @copyright   DraiWiki, 2017
+ * @license     Apache 2.0
+ */
+
+define('DraiWiki', 1);
+
+require 'public/index.php';
