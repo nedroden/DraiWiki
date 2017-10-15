870506c60580e6c87de08d6febd7a12d364eb5ca
stefanos vichas <stefanosvchs@gmail.com>
2017-10-15 21:36:38 +0300

    Adding greek language to draiwiki

---
+++ locales/el_GR/article.locale.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * en_US
+ * DraiWiki language pack
+ *
+ * DraiWiki:
+ * @author		Robert Monden
+ * @copyright	DraiWiki development team, 2017
+ * @version		1.0 Alpha 1
+ * @license		Apache 2.0
+ *
+ * Language pack:
+ * @dialect		General American English
+ * @country 	United States
+ * @translator	Robert Monden
+ * @license		Apache 2.0
+ */
+
+return [
+    'editing' => 'Επεξεργασία άρθρου: ',
+    'deleting' => 'Διαγραφή άρθρου',
+    'new_article' => 'Νέο άρθρο',
+    'history' => 'Προβολή ιστορικού άρθρων',
+
+    'last_updated_by' => 'Τελευταία ενημέρωση %s στις %s',
+
+    'username' => 'Χρήστης',
+    'updated' => 'Ημερομινία',
+
+    'historical_version' => 'Προβάλετε μια ιστορική έκδοση αυτού του άρθρου. Για να μεταβείτε στην τρέχουσα έκδοση, κάντε κλικ στον σύνδεσμο \'ανάγνωση άρθρου \' στην πλαϊνή γραμμή',
+
+    'assign_translations' => 'Αναθέστε τις μεταφράσεις',
+    'assign_translations_what' => 'Αυτή η σελίδα σάς επιτρέπει να συνδέσετε ένα άρθρο σε άρθρα σε άλλες γλώσσες. Αυτοί οι σύνδεσμοι χρησιμοποιούνται για τον προσδιορισμό ενός συγκεκριμένου άρθρου ως μετάφραση άλλου. Αναζητήστε ένα άρθρο και κάντε κλικ στον τίτλο και το λογισμικό wiki προσθέτει αυτόματα το τρέχον άρθρο στην ομάδα μετάφρασης του επιλεγμένου άρθρου.'
+];
---
+++ locales/el_GR/editor.locale.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * en_US
+ * DraiWiki language pack
+ *
+ * DraiWiki:
+ * @author		Robert Monden
+ * @copyright	DraiWiki development team, 2017
+ * @version		1.0 Alpha 1
+ * @license		Apache 2.0
+ *
+ * Language pack:
+ * @dialect		General American English
+ * @country 	United States
+ * @translator	Robert Monden
+ * @license		Apache 2.0
+ */
+
+return [
+    'title' => 'Τίτλος',
+    'save' => 'Αποθήκευση',
+
+    'title_too_short' => 'Ο τίτλος είναι πολύ μικρός. Οι τίτλοι πρέπει να έχουν μήκος τουλάχιστον %u χαρακτήρων',
+    'title_too_long' => 'Ο τίτλος είναι υπερβολικός. Οι τίτλοι πρέπει να έχουν το πολύ %u χαρακτήρες',
+    'title_already_in_use' => 'Ο τίτλος χρησιμοποιείται ήδη',
+
+    'body_too_short' => 'Το άρθρο είναι πολύ σύντομο. Τα άρθρα πρέπει να έχουν τουλάχιστον %u χαρακτήρες',
+    'body_too_long' => 'Το άρθρο είναι πολύ μακρύ. Τα άρθρα πρέπει να έχουν το πολύ %u χαρακτήρες',
+
+    'invalid_id' => 'Μη έγκυρο αναγνωριστικό άρθρου',
+    'article_not_found' => 'Το άρθρο δεν βρέθηκε',
+
+    'title_no_html' => 'Οι τίτλοι των άρθρων δεν μπορούν να περιέχουν HTML'
+];
